e4f6cb20dc6551fa9b51d065241011a7aae05464
usuns <usunsun38@naver.com>
2025-11-27 04:31:42 +0900

    1. ㅁㅁ

--- app/src/main/java/com/example/filter/activities/MainActivity.java
+++ app/src/main/java/com/example/filter/activities/MainActivity.java
@@ -146,7 +146,7 @@ public class MainActivity extends BaseActivity {
         recyclerView.addItemDecoration(new GridSpaceItemDecoration(2, dp(12), dp(18)));
 
         setFilterButtons(true, false, false, false); // recommend 활성화
-        recommend.performClick();
+        loadRecommendFilters();
 
         // 데이터 변경 감지 및 뷰 갱신
         filterAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
